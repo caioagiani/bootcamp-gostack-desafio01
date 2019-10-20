e6fe14726134362e24aa1af5a7ba70883ef0a952
caio-agiani <caio.agiani14@gmail.com>
2019-10-20 16:02:34 -0300

    GoStack01

---
+++ README.md
@@ -0,0 +1,19 @@
+## Desafio 01 - Bootcamp GoStack
+
+#### :pencil2: OBJETIVO:
+
+Desenvolver um Crud simples para projetos;
+
+#### :pushpin: ROTAS:
+
+- [x] ``GET /projects``: Ver todos os projetos cadastrados;
+- [x] ``POST /projects/:id``: Cadastrar o objeto contendo os dados do projeto;
+- [x] ``PUT /projects/:id``: Atualizar nomes de projetos;
+- [x] ``POST /projects/:id/tasks``: Adicionar tarefas aos projetos;
+- [x] ``DELETE /projects/:id``: Remover projeto pelo id;
+
+### Midlewares
+- [x] Desenvolver um middleware que verifica o id do projeto vindo dos parametros de rotas;
+- [x] Middleware global que imprimi um `console.log` com a contagem de quantas requisições foram feitas;
+
+Feito com :hearts: by Caio Agiani
---
+++ src/index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+
+const app = express();
+
+let projects = [];
+let request = 0;
+
+app.use(express.json());
+
+/** Middlewares */
+
+/** Check projects exists */
+const checkList = (req, res, next) => {
+    const { id } = req.params;
+    const project = projects.find(p => p.id == id);
+
+    if (!project) return res.status(400).json({ error: 'Project does not exists' });
+
+    req.project = project;
+
+    return next();
+}
+
+/** Generate log  */
+const changeLogs = (req, res, next) => {
+    request++;
+
+    console.log(`${req.method} - ${req.url} :: Request Number: ${request}`);
+
+    return next();
+}
+
+/** Route edit id projects */
+app.put('/projects/:id', checkList, changeLogs, (req, res) => {
+    const { title } = req.body;
+
+    req.project.title = title;
+
+    return res.json(req.project);
+});
+
+/** Route create projects */
+app.post('/projects', changeLogs, (req, res) => {
+    const { id, title } = req.body;
+
+    projects.push({ id: id, title: title, tasks: [] });
+
+    return res.json(projects);
+});
+
+/** Route list projects */
+app.get('/projects', (req, res) => {
+    res.json(projects);
+});
+
+app.delete('/projects/:id', checkList, changeLogs, (req, res) => {
+    projects.splice(req.projectIndex, 1);
+    
+    return res.json(projects);
+});
+
+app.post('/projects/:id/tasks', checkList, changeLogs, (req, res) => {
+    const { titleTask } = req.body;
+
+    req.project.tasks.push(titleTask);
+
+    return res.send(req.project);
+});
+
+app.listen(3333, () => {
+    console.log('Server started on port 3333');
+})
